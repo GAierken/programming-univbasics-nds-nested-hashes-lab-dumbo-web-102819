26c16c18785c610f4e0917120e66c2362a4ab0c8
Guligena Aierken <guligena_aierken@163.com>
2019-10-24 01:43:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,6 +31,6 @@ end
 def aoh_update(aoh, row, key, new_value)
   aoh[row][key]=new_value
   # Update the AoH data at row and key to have the value of new_value
-  aoh[row][key]
+  aoh
   # Return the updated AoH
 end
